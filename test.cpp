2de1078f7a2dd90f53cff19ada325b4a754739ee
Cougtt <nguyennhatnhan2007@gmail.com>
2025-12-26 13:48:25 +0700

    Branch edit :6

--- test.cpp
+++ test.cpp
@@ -3,5 +3,6 @@
 int main() {
     std::cout << "Hello World" << std::endl;
     std::cout << "Happy Holiday! This is my first time experiencing GitHub!" << std::endl;
+    std::cout << "This code in on a branch_vsc" << std::endl;
     return 0;
 }
